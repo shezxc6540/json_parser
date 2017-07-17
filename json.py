6e504832b8c555d62a664b02039bd1ea4ee70a18
liuyuan <gzliuyuan2017@corp.netease.com>
2017-07-17 20:36:00 +0800

    finish functions but not finish handle exception and not debug

--- json.py
+++ json.py
@@ -1,8 +1,4 @@
 
-
-def has_six_chars(c):
-    return c == '[' or c == '{' or c == ']' or c == '}' or c == ':' or c == ','
-
 class MyError(Exception):
     def __init__(self):
         pass
@@ -26,197 +22,133 @@ class JsonParser:
                 new_s += c
         return new_s
 
+    def parser_string(self, s):
+        if s[0] != '"':
+            raise MyError
+        temp_index = 1
+        while temp_index < len(s) and s[temp_index] != '"':
+            temp_index = temp_index + 1
+        if temp_index == len(s):
+            raise MyError
+        else:
+            return s[1:temp_index], s[temp_index + 1 :]
+
+    def is_digit(self, s):
+        if s[0] >= '0' and s[0] <= '9':
+            return True
+        elif s[0] == 'e' or s[0] == 'E':
+            return True
+        elif s[0] == '+' or s[0] == '-' or s[0] == '.':
+            return True
+        else:
+            return False
+
+    def parser_value(self, s):
+        if s[0] == '{':
+            temp_index = 1
+            count_bracket = 0
+            while temp_index < len(s):
+                if s[temp_index] == '{':
+                    count_bracket = count_bracket + 1
+                elif s[temp_index] == '}':
+                    if count_bracket == 0:
+                        break
+                    else:
+                        count_bracket = count_bracket - 1
+                temp_index = temp_index + 1
+            if temp_index == len(s):
+                raise MyError
+            else:
+                return self.parser_object(s[:temp_index + 1]), s[temp_index + 1 :]
+        elif s[0] == '[':
+            temp_index = 1
+            count_bracket = 0
+            while temp_index < len(s):
+                if s[temp_index] == '[':
+                    count_bracket = count_bracket + 1
+                elif s[temp_index] == ']':
+                    if count_bracket == 0:
+                        break
+                    else:
+                        count_bracket = count_bracket - 1
+                temp_index = temp_index + 1
+            if temp_index == len(s):
+                raise MyError
+            else:
+                return self.parser_array(s[:temp_index + 1]), s[temp_index + 1 :]
+        elif s[0] == '"':
+            return self.parser_string(s)
+        elif s[0] == 't':
+            if len(s) >= 4 and s[1:4] == 'rue':
+                return True, s[4:]
+        elif s[0] == 'f':
+            if len(s) >= 5 and s[1:5] == 'alse':
+                return False, s[5:]
+        elif s[0] == 'n':
+            if len(s) >= 4 and s[1:4] = 'ull':
+                return None, s[4:]
+        elif (s[0] >= '0' and s[0] <= '9') or s[0] =='-':
+            if(s[0] == '0' and len(s) > 1):
+                raise  MyError
+            temp_index = 1
+            while temp_index < len(s) and self.is_digit(s[temp_index]):
+                temp_index = temp_index + 1
+            if temp_index == len(s):
+                raise MyError
+            else:
+                return self.parser_number(s[0:temp_index]), s[temp_index:]
+        else:
+            raise MyError
+
     def parser_number(self, s):
         res = 0
         try:
-            res = float(s)
-            try:
-                temp = int(s)
-                res = temp
-            except:
-                print('It is a integer')
+            res = int(s)
+            return res
         except:
-            print('number error')
+            try:
+                res = float(s)
+                return res
+            except:
+                raise MyError
 
     def parser_array(self, s):
         try:
-            sum_length = len(s)
-            if sum_length < 2 or s[0] != '[' or s[-1] != ']':
+            if len(s) < 2 or s[0] != '[' or s[-1] != ']':
                 raise  MyError
             index = 1
             temp_list = list()
-            while index < sum_length - 1:
-                if s[index] == '{':
-                    if pre_char != ':':
-                        raise  MyError
-                    temp_index = index + 1
-                    count_bracket = 0
-                    while temp_index < (sum_length - 1):
-                        if s[temp_index] == '{':
-                            count_bracket = count_bracket + 1
-                        elif s[temp_index] == '}':
-                            if count_bracket == 0:
-                                break
-                            else:
-                                count_bracket = count_bracket - 1
-                    if temp_index == (sum_length - 1):
-                        raise MyError
-                    else:
-                        temp_list.append(self.parser_object(s[index:temp_index + 1]))
-                        index = temp_index + 1
-                        single_finished = True
-                elif s[index] == '[':
-                    if pre_char != ':':
-                        raise  MyError
-                    temp_index = index + 1
-                    count_bracket = 0
-                    while temp_index < (sum_length - 1):
-                        if s[temp_index] == '[':
-                            count_bracket = count_bracket + 1
-                        elif s[temp_index] == ']':
-                            if count_bracket == 0:
-                                break
-                            else:
-                                count_bracket = count_bracket - 1
-                    if temp_index == (sum_length - 1):
-                        raise MyError
-                    else:
-                        temp_list.append(self.parser_object(s[index:temp_index + 1]))
-                        index = temp_index + 1
-                        single_finished = True
-                elif s[index] == '"':
-                    if pre_char != ',' or index != 1 or pre_char != ':':
-                        raise MyError
-                    temp_index = index + 1
-                    while temp_index < (sum_length - 1) and s[temp_index] != '"':
-                        temp_index = temp_index + 1
-                    if temp_index == (sum_length - 1):
-                        raise MyError
-                    else:
-                        temp_list.append(s[index:temp_index + 1])
-                        index = temp_index + 1
-                elif s[index] == ',':
-                    if not single_finished:
-                        raise MyError
-                    handle_key = True
-                    pre_char = ','
-                elif s[index] == 't':
-                    pass
-                elif s[index] == 'f':
-                    pass
-                elif s[index] == 'n':
-                    pass
-                elif (s[index] >= '0' and s[index] <= '9') or s[index] =='-':
-                    if pre_char != ',' or index != 1:
-                        raise MyError
-                    temp_index = index + 1
-                    while temp_index < (sum_length - 1) and (s[temp_index] != ',' and s[temp_index] != '}'):
-                        temp_index = temp_index + 1
-                    if temp_index == (sum_length - 1):
-                        raise MyError
-                    else:
-                        temp_list.append(self.parser_number(s[index]))
-                        index = temp_index + 1
+            while True:
+                temp_value, s = self.parser_value(s[1:])
+                temp_list.append(temp_value)
+                if len(s) > 1 and s[0] == ',':
+                    s = s[1:]
+                elif s == '}':
+                    return temp_list
+                else:
+                    raise MyError
             return temp_list
         except:
+            raise MyError
 
     def parser_object(self, s):
         try:
-            sum_length = len(s)
-            if sum_length < 2 or s[0] != '{' or s[-1] != '}':
+            if len(s) < 2 or s[0] != '{' or s[-1] != '}':
                 raise MyError
             temp_dict = dict()
             temp_key = str()
-            handle_key = True
-            single_finished = False
-            half_finished = False
-            index = 1
-            pre_char = None
-            while index < sum_length - 1:
-                if s[index] == '{':
-                    if pre_char != ':':
-                        raise  MyError
-                    temp_index = index + 1
-                    count_bracket = 0
-                    while temp_index < (sum_length - 1):
-                        if s[temp_index] == '{':
-                            count_bracket = count_bracket + 1
-                        elif s[temp_index] == '}':
-                            if count_bracket == 0:
-                                break
-                            else:
-                                count_bracket = count_bracket - 1
-                    if temp_index == (sum_length - 1):
-                        raise MyError
+            while True:
+                temp_key, s = self.parser_string(s[1:])
+                if s[0] == ':' and len(s) > 1:
+                    temp_dict[temp_key], s = self.dump_value(s[1:])
+                    if s[0] == ',' and len(s) > 1:
+                        s = s[1:]
+                    elif s == '}':
+                        return temp_dict
                     else:
-                        temp_dict[temp_key] = self.parser_object(s[index:temp_index + 1])
-                        index = temp_index + 1
-                        single_finished = True
-                elif s[index] == '[':
-                    if pre_char != ':':
-                        raise  MyError
-                    temp_index = index + 1
-                    count_bracket = 0
-                    while temp_index < (sum_length - 1):
-                        if s[temp_index] == '[':
-                            count_bracket = count_bracket + 1
-                        elif s[temp_index] == ']':
-                            if count_bracket == 0:
-                                break
-                            else:
-                                count_bracket = count_bracket - 1
-                    if temp_index == (sum_length - 1):
                         raise MyError
-                    else:
-                        temp_dict[temp_key] = self.parser_object(s[index:temp_index + 1])
-                        index = temp_index + 1
-                        single_finished = True
-                elif s[index] == '"':
-                    if pre_char != ',' or index != 1 or pre_char != ':':
-                        raise MyError
-                    temp_index = index + 1
-                    while temp_index < (sum_length - 1) and s[temp_index] != '"':
-                        temp_index = temp_index + 1
-                    if temp_index == (sum_length - 1):
-                        raise MyError
-                    else:
-                        if handle_key:
-                            temp_key = s[index:temp_index + 1]
-                            half_finished = True
-                        else:
-                            temp_dict[temp_key] = s[index:temp_index + 1]
-                            single_finished = True
-                        index = temp_index + 1
-                elif s[index] == ',':
-                    if not single_finished:
-                        raise MyError
-                    handle_key = True
-                elif s[index] == ':':
-                    if not half_finished:
-                        raise MyError
-                    handle_key = False
-                elif s[index] == 't':
-                    pass
-                elif s[index] == 'f':
-                    pass
-                elif s[index] == 'n':
-                    pass
-                elif (s[index] >= '0' and s[index] <= '9') or s[index] =='-':
-                    if pre_char != ',' or index != 1:
-                        raise MyError
-                    temp_index = index + 1
-                    while temp_index < (sum_length - 1) and (s[temp_index] != ',' and s[temp_index] != '}'):
-                        temp_index = temp_index + 1
-                    if temp_index == (sum_length - 1):
-                        raise MyError
-                    else:
-                        temp_dict[temp_key] = self.parser_number(s[index])
-                        index = temp_index + 1
                 else:
                     raise MyError
-                pre_char = s[index]
-                index = index + 1
-            return temp_dict
         except:
             raise MyError
 
@@ -255,25 +187,33 @@ class JsonParser:
             temp_str += ','
         return temp_str[:-1] + '}'
 
-    def main_parser(self, s):
+    def loads(self, s):
         s = self.handle_whitespace(s)
         try:
-            return self.parser_object(s)
+            _data = self.parser_object(s)
         except:
             raise MyError
-
-
-    def loads(self, s):
-        pass
 
     def dumps(self):
         return self.dump_object(self._data)
 
     def load_file(self, f):
-        pass
+        try:
+            with open(f, "r") as read_json:
+                json_str = ''.join(read_json.readlines())
+                self.loads(json_str)
+        except IOError:
+            print("File read error!")
+
 
     def dump_file(self, f):
-        pass
+        try:
+            out1 = open(f, "w")
+            out1.write(self.dumps())
+        except IOError:
+            print("File write error!")
+        finally:
+            out1.close()
 
     def load_dict(self, d):
         _data = d
