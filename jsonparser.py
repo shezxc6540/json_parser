b8e199a7ec977b67ddf04c80d1896a82ec7f7558
liuyuan <gzliuyuan2017@corp.netease.com>
2017-07-27 15:07:11 +0800

    correct some bugs about unicode but not finish optimizing algorithm of parser

--- jsonparser.py
+++ jsonparser.py
@@ -2,21 +2,26 @@
 # -*- coding: utf-8 -*-
 
 
+ACCEPTED_SPACE_CHARS = '{[}],:'
+ESCAPE_DIR_PARSER = {'t': '\t', 'r': '\r', 'n': '\n', 'f': '\f', 'b': '\b'}
+ESCAPE_DIR_DUMP = {'"': '"', '\\': '\\', '/': '/', '\b': 'b', '\f': 'f', '\n': 'n',
+                   '\r': 'r', '\t': 't'}
+LEGAL_DIGIT = '0123456789eE+-.'
 class JsonError(Exception):
+    """自定义异常的基类"""
     def __init__(self):
         pass
 
 
-class ParserError(JsonParser):
+class ParserError(JsonError):
     """自定义异常类。
 
     在解析Json字符串遇到错误时抛出该异常"""
     def __init__(self, error_type):
-        Exception.__init__(self)
         self.error_type = error_type
 
 
-class DumpError(JsonParser):
+class DumpError(JsonError):
     def __init__(self):
         pass
 
@@ -33,7 +38,7 @@ class JsonParser(object):
 
         若遇到JSON格式错误的应该抛出异常。为简便考虑，JSON的最外层假定只为Object"""
         # 预处理字符串中的空格
-        s = self.handle_whitespace(s)
+        s = self.remove_space(s)
         try:
             self._data = self.parser_object(s)
         except ParserError as pe:
@@ -43,30 +48,25 @@ class JsonParser(object):
         """将实例中的内容转成JSON格式返回。
 
         """
-        return self.dump_object(self._data)
+        try:
+            return self.dump_object(self._data)
+        except DumpError:
+            print("Dump Error!")
 
     def load_file(self, f):
         """从文件中读取JSON格式数据，f为文件路径。
 
         Json解析时异常处理，文件操作失败抛出异常"""
-        try:
-            with open(f, "r") as read_json:
-                json_str = read_json.read()
-                self.loads(json_str)
-        except IOError:
-            print("File read error!")
+        with open(f, "r") as read_json:
+            json_str = read_json.read()
+            self.loads(json_str)
 
     def dump_file(self, f):
         """将实例中的内容以JSON格式存入文件。
 
         文件若存在则覆盖，文件操作失败抛出异常"""
-        try:
-            out1 = open(f, "w")
+        with open(f, "w") as out1:
             out1.write(self.dumps())
-        except IOError:
-            print("File write error!")
-        finally:
-            out1.close()
 
     def load_dict(self, d):
         """从dict中读取数据，存入实例中，若遇到不是字符串的key则忽略。
@@ -109,34 +109,7 @@ class JsonParser(object):
             if isinstance(k, str):
                 self._data[k] = self.deep_copy_value(v)
 
-    def is_digit(self, s):
-        """判断一个字符是否为合法数字中可能出现的字符。
-
-        """
-        if s[0] >= '0' and s[0] <= '9':
-            return True
-        elif s[0] in ('e', 'E', '+', '-', '.'):
-            return True
-        else:
-            return False
-
-    def has_six_chars(self, c):
-        """检验字符c是否为两边允许出现空格的字符。
-
-        返回True或False"""
-        return c in ('{', '[', '}', ']', ',', ':')
-
-    def is_sixteen_digit(self, c):
-        if c >= '0' and c <= '9':
-            return True
-        elif c >= 'a' and c <= 'f':
-            return True
-        elif c >= 'A' and c <= 'F':
-            return True
-        else:
-            return False
-
-    def handle_whitespace(self, s):
+    def remove_space(self, s):
         """对于输入的一个字符串，将允许去除的空格全部去掉，以便于下一步操作。
 
         初始化一个新串，遍历旧串， 如果是空格，查看新串末尾是否为允许前后出现空
@@ -144,12 +117,12 @@ class JsonParser(object):
         末尾的空格都去除掉；如果为其他，则直接加入新串中。"""
         new_s = ''   # new string
         for c in s:
-            if self.has_six_chars(c):
+            if c in ACCEPTED_SPACE_CHARS:
                 new_s = new_s.rstrip()
                 new_s += c
             elif c.isspace():
-                # The new string is empty or the end of new string is not the six chars
-                if len(new_s) == 0 or self.has_six_chars(new_s[-1]) is not True:
+                # The new string is empty or the end of new string is not the accepted space chars
+                if len(new_s) == 0 or new_s[-1] not in ACCEPTED_SPACE_CHARS:
                     new_s += c
             else:
                 new_s += c
@@ -168,22 +141,17 @@ class JsonParser(object):
                 temp_index += 1
                 if temp_index == len(s):
                     raise ParserError('string')
-                if s[temp_index] == 't':
-                    new_s += '\t'
-                elif s[temp_index] == 'r':
-                    new_s += '\r'
-                elif s[temp_index] == 'n':
-                    new_s += '\n'
-                elif s[temp_index] == 'f':
-                    new_s += '\f'
-                elif s[temp_index] == 'b':
-                    new_s += '\b'
+                if s[temp_index] in ESCAPE_DIR_PARSER:
+                    new_s += ESCAPE_DIR_PARSER[s[temp_index]]
                 elif s[temp_index] in ('/', '"', '\\'):
                     new_s += s[temp_index]
                 elif s[temp_index] == 'u':
-                    if (temp_index + 4) < len(s) and self.is_sixteen_digit(s[temp_index + 1]) and self.is_sixteen_digit(s[temp_index + 2]) and self.is_sixteen_digit(s[temp_index + 3]) and self.is_sixteen_digit(s[temp_index + 4]):
-                        new_s += s[temp_index - 1:temp_index + 5]
-                        temp_index += 4
+                    if (temp_index + 4) < len(s):
+                        try:
+                            new_s += s[temp_index - 1:temp_index + 5].decode('raw_unicode_escape')
+                            temp_index += 4
+                        except ValueError:
+                            raise ParserError('string')
                     else:
                         raise ParserError('string')
                 else:
@@ -242,26 +210,17 @@ class JsonParser(object):
         elif s[0] == '"':
             return self.parser_string(s)
         # 第一个字符为t，认为是true
-        elif s[0] == 't':
-            if len(s) >= 4 and s[1:4] == 'rue':
-                return True, s[4:]
-            else:
-                raise ParserError('true')
+        elif s.startswith('true'):
+            return True, s[4:]
         # 第一个字符为f，认为是false
-        elif s[0] == 'f':
-            if len(s) >= 5 and s[1:5] == 'alse':
-                return False, s[5:]
-            else:
-                raise ParserError('false')
-        elif s[0] == 'n':
-            if len(s) >= 4 and s[1:4] == 'ull':
-                return None, s[4:]
-            else:
-                raise ParserError('null')
+        elif s.startswith('false'):
+            return False, s[5:]
+        elif s.startswith('null'):
+            return None, s[4:]
         # 第一个字符为数字或者是负号时，认为是number
-        elif (s[0] >= '0' and s[0] <= '9') or s[0] == '-':
+        elif ('0' <= s[0] <= '9') or s[0] == '-':
             temp_index = 1
-            while temp_index < len(s) and self.is_digit(s[temp_index]):
+            while temp_index < len(s) and s[temp_index] in LEGAL_DIGIT:
                 temp_index += 1
             if temp_index == len(s):
                 raise ParserError('number')
@@ -274,18 +233,15 @@ class JsonParser(object):
         """解析Json字符串中的number类型
 
         利用int()和float()函数和try/except语法来判断是否出错和解析字符串。"""
-        if (s[0] == '0' and len(s) > 1):
+        if s[0] == '0' and len(s) > 1 and s[1] != '.':
             raise ParserError('number')
-        res = 0
         try:
             # 此处如果出现ValueError，则可能是float或者解析错误
-            res = int(s)
-            return res
+            return int(s)
         except ValueError:
             try:
                 # 此处如果出现ValueError， 则可以判断是解析错误，不合法。
-                res = float(s)
-                return res
+                return float(s)
             except ValueError:
                 raise ParserError('number')
 
@@ -335,43 +291,27 @@ class JsonParser(object):
         """将str类型转换为Json字符串中带双引号的string
 
         """
+        s = s.encode('unicode-escape').decode('string_escape')
         new_s = '"'
         for i in range(0, len(s)):
-            if s[i] == '"':
-                new_s += '\\'
+            if (i + 1) < len(s) and s[i:i + 2] == '\u':
                 new_s += s[i]
-            elif s[i] == '\b':
+            elif s[i] in ESCAPE_DIR_DUMP:
                 new_s += '\\'
-                new_s += 'b'
-            elif s[i] == '\f':
-                new_s += '\\'
-                new_s += 'f'
-            elif s[i] == '\n':
-                new_s += '\\'
-                new_s += 'n'
-            elif s[i] == '\r':
-                new_s += '\\'
-                new_s += 'r'
-            elif s[i] == '\t':
-                new_s += '\\'
-                new_s += 't'
-            elif s[i] == '\\' and s[i + 1] != 'u':
-                new_s += '\\'
-                new_s += s[i]
+                new_s += ESCAPE_DIR_DUMP[s[i]]
             else:
                 new_s += s[i]
         new_s += '"'
-        return unicode(new_s)
+        return new_s
 
     def dump_value(self, v):
         """判断v对应的类型是什么，转换为对应的类型或值
 
         """
-        if isinstance(v, bool):
-            if v:
-                return 'true'
-            else:
-                return 'false'
+        if v is True:
+            return 'true'
+        elif v is False:
+            return 'false'
         elif isinstance(v, dict):
             return self.dump_object(v)
         elif isinstance(v, list):
@@ -383,33 +323,22 @@ class JsonParser(object):
         elif v is None:
             return 'null'
         else:
-            print type(v)
+            raise DumpError
 
     def dump_array(self, l):
         """将list转换为Json字符串中的array类型。
 
         注意空list的特殊处理。"""
-        if len(l) == 0:
-            return u'[]'
-        temp_str = '['
-        for it in l:
-            temp_str += self.dump_value(it)
-            temp_str += ','
-        return temp_str[:-1] + ']'
+        return '[%s]' % (', '.join(map(self.dump_value, l)))
 
     def dump_object(self, d):
         """将dict转换为Json字符串中的object类型。
 
         注意空dict的特殊处理。"""
-        if len(d) == 0:
-            return u'{}'
-        temp_str = '{'
-        for k,v in d.iteritems():
-            temp_str += self.dump_string(k)
-            temp_str += ':'
-            temp_str += self.dump_value(v)
-            temp_str += ','
-        return temp_str[:-1] + '}'
+        return unicode(
+            '{%s}' % (', '.join(map(lambda x, y: self.dump_string(x) + ': ' + self.dump_value(y),
+                      d.keys(), d.values())))
+        )
 
     def deep_copy_value(self, v):
         """对于value进行深拷贝。
